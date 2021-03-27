c8ced9221e204d2482bfc0f216b69e8caa59098d
Kalaes <79618686+Kalaes@users.noreply.github.com>
2021-03-27 14:48:24 -0300

    Atualização de Stats

--- Base de Players/src/Kalaes/Raphael/Main.java
+++ Base de Players/src/Kalaes/Raphael/Main.java
@@ -26,60 +26,60 @@ public class Main {
 
 
         p1.name = "raphael_kalaes";
-        p1.kills = 39;
-        p1.assists = 18;
-        p1.deaths = 36;
-        p1.wins = 5;
+        p1.kills = 48;
+        p1.assists = 19;
+        p1.deaths = 43;
+        p1.wins = 6;
         p1.losses = 0;
-        p1.kd = 1.08;
+        p1.kd = 1.12;
 
         p2.name = "Maranhao_007";
-        p2.kills = 70;
-        p2.assists = 13;
-        p2.deaths = 49;
+        p2.kills = 74;
+        p2.assists = 15;
+        p2.deaths = 59;
         p2.wins = 2;
-        p2.losses = 3;
-        p2.kd = 1.43;
+        p2.losses = 4;
+        p2.kd = 1.25;
 
         p3.name = "BrookingSkulls";
-        p3.kills = 38;
-        p3.assists = 12;
-        p3.deaths = 28;
-        p3.wins = 1;
+        p3.kills = 48;
+        p3.assists = 16;
+        p3.deaths = 34;
+        p3.wins = 2;
         p3.losses = 2;
-        p3.kd = 1.36;
+        p3.kd = 1.41;
 
         p4.name = "Rafa_Satriani";
-        p4.kills = 51;
-        p4.assists = 22;
-        p4.deaths = 48;
+        p4.kills = 61;
+        p4.assists = 24;
+        p4.deaths = 55;
         p4.wins = 4;
-        p4.losses = 1;
-        p4.kd = 1.06;
+        p4.losses = 2;
+        p4.kd = 1.11;
 
         p5.name = "Rmazaro";
-        p5.kills = 23;
-        p5.assists = 9;
-        p5.deaths = 48;
+        p5.kills = 25;
+        p5.assists = 11;
+        p5.deaths = 55;
         p5.wins = 1;
-        p5.losses = 4;
-        p5.kd = 0.48;
+        p5.losses = 5;
+        p5.kd = 0.45;
 
         p6.name = "FTK-Gago";
-        p6.kills = 47;
-        p6.assists = 18;
-        p6.deaths = 40;
-        p6.wins = 1;
+        p6.kills = 59;
+        p6.assists = 21;
+        p6.deaths = 46;
+        p6.wins = 2;
         p6.losses = 4;
-        p6.kd = 1.18;
+        p6.kd = 1.28;
 
         p7.name = "rfv-_-_-_-_-mw3";
-        p7.kills = 20;
-        p7.assists = 4;
-        p7.deaths = 19;
+        p7.kills = 33;
+        p7.assists = 7;
+        p7.deaths = 26;
         p7.wins = 1;
-        p7.losses = 1;
-        p7.kd = 1.05;
+        p7.losses = 2;
+        p7.kd = 1.27;
 
         p8.name = "calogamers2001";
         p8.kills = 8;
@@ -106,12 +106,12 @@ public class Main {
         p10.kd = 0.91;
 
         p11.name = "Im_The_New_Wat";
-        p11.kills = 42;
-        p11.assists = 15;
-        p11.deaths = 36;
+        p11.kills = 49;
+        p11.assists = 17;
+        p11.deaths = 45;
         p11.wins = 2;
-        p11.losses = 2;
-        p11.kd = 1.17;
+        p11.losses = 3;
+        p11.kd = 1.09;
 
         p12.name = "yPsYcho-CP";
         p12.kills = 8;
@@ -146,12 +146,12 @@ public class Main {
         p15.kd = 0.72;
 
         p16.name = "yMrF1Vve-_-";
-        p16.kills = 8;
-        p16.assists = 4;
-        p16.deaths = 6;
-        p16.wins = 1;
+        p16.kills = 14;
+        p16.assists = 9;
+        p16.deaths = 13;
+        p16.wins = 2;
         p16.losses = 0;
-        p16.kd = 1.33;
+        p16.kd = 1.08;
 
         p17.name = "gui_tanakac";
         p17.kills = 11;
@@ -162,12 +162,12 @@ public class Main {
         p17.kd = 1.57;
 
         p18.name = "Galo_Sniper";
-        p18.kills = 5;
-        p18.assists = 1;
-        p18.deaths = 5;
-        p18.wins = 1;
+        p18.kills = 9;
+        p18.assists = 3;
+        p18.deaths = 14;
+        p18.wins = 2;
         p18.losses = 0;
-        p18.kd = 1.00;
+        p18.kd = 0.64;
 
 
 
